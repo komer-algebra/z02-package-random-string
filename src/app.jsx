ba63ae39860fdf3c71a072356d415a4e4d5e3da9
Komer @ Algebra <komersmile@gmail.com>
2023-09-04 20:16:42 +0200

    package name changed

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,9 @@ import './app.css'
 import randomString from 'random-string'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Package random string</h1>
+      <h1>Package random-string</h1>
       <p>Evo ga: {randomString({length: 20})}</p>
     </>
   )
